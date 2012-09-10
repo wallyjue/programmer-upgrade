ea43e916f6a838f884b321a7aea08103cd1284af
wallyjue@gmail.com <wallyjue@gmail.com@e9b1c768-ab86-e322-5f66-cf6bd6dca5a6>
2012-09-10 16:12:21 +0000

    add append and destroy

--- darray.c
+++ darray.c
@@ -1,5 +1,47 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "darray.h"
 
+void print_content_char(DArray* darray, void* data)
+{
+	printf("%c\n", darray->element);
+}
+
+Ret darray_for_each( DArray* darray, DArrayVisitFunc function, void* data)
+{
+	if( darray == NULL)
+		return RET_FAIL;
+	
+	size_t array_size = 0;
+	for( array_size = 0; array_size < darray->size ; array_size++)
+	{
+		function( darray, data);
+	}
+	return RET_OK;
+}
+
+Ret darray_append( DArray* thiz, size_t index, void* data)
+{
+	if( thiz == NULL )
+	{
+		return RET_FAIL;
+	}	
+	
+	void* buffer = (void*) malloc( index * sizeof(void*) + (thiz->size) * sizeof(void*));
+	mempcpy( buffer, thiz->element, thiz->size);
+	mempcpy( buffer + sizeof(size_t)*(thiz->size), data, index*sizeof(size_t));
+	thiz->size += index;
+	free( data);
+	data = NULL;
+	free( thiz->element );
+	thiz->element = NULL;
+	thiz->element = buffer;
+
+	return RET_OK;
+}
 
 Ret darray_destory( DArray* thiz)
 {
@@ -7,7 +49,7 @@ Ret darray_destory( DArray* thiz)
 		return RET_FAIL;
 		
 	thiz->size = 0;
-	if( thiz->element == NULL)
+	if( thiz->element != NULL)
 	{
 		free( thiz->element );
 		thiz->element = NULL;
@@ -24,6 +66,7 @@ DArray* darray_create( DArrayDestoryFunc darray_destroy, void* ctx)
 	return darray;
 }
 
+/*
 Ret darray_append( DArray* thiz, size_t index, void* ctx)
 {
 	if( thiz == NULL)
@@ -37,3 +80,4 @@ Ret darray_append( DArray* thiz, size_t index, void* ctx)
 	}
 	return RET_OK;
 }
+*/
