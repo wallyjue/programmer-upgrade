19fc8d9f44e5f80f39f2cbc1e14e303cfb2447f2
wallyjue@gmail.com <wallyjue@gmail.com@e9b1c768-ab86-e322-5f66-cf6bd6dca5a6>
2012-09-10 15:01:21 +0000

    add darray functions

--- trunk/main.c
+++ trunk/main.c
@@ -3,35 +3,13 @@
 #include <string.h>
 #include <ctype.h>
 #include "dlist.h"
-
 #include "darray.h"
 
 int main(int argc, char** argv)
 {
-	//CU_pSuite pSuite = NULL;
-	//if (CUE_SUCCESS != CU_initialize_registry())
-		//return CU_get_error();
-		
-	NodeList* nodelist = (NodeList*) malloc( sizeof( NodeList ));
-	
-	Node* node1 = InitNode( 1, "chaojue" );
-	DListAddNode( nodelist, node1 );
-	Node* node2 = InitNode( 2, "chang" );
-	DListAddNode( nodelist, node2 );
-	Node* node3 = InitNode( 3, "kakasi" );
-	DListAddNode( nodelist, node3 );
+	DArray* darray = (DArray*) malloc( sizeof( DArray ));
 	
 	
-	
-	DListTraverse( nodelist, print_content, NULL );
-	DListTraverse( nodelist, content_to_upper, NULL );
-	DListTraverse( nodelist, print_content, NULL );
-	
-	FreeNode( node1 );
-	FreeNode( node2 );
-	FreeNode( node3 );
-	free( nodelist);
-	nodelist = NULL;
-	//CU_cleanup_registry();
+	darray_destory( darray );
 	return 0;
 }
