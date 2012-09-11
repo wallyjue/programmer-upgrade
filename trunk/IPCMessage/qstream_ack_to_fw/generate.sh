844a89b3c587a40d19998b75d383a7f361119b3b
wallyjue@gmail.com <wallyjue@gmail.com@e9b1c768-ab86-e322-5f66-cf6bd6dca5a6>
2012-09-11 11:48:45 +0000

    rm IPCmessage

--- trunk/IPCMessage/qstream_ack_to_fw/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp ../src/ipc_message.c files
-cp ../src/ipc_message.h files
-python ../tool/ipc_server_c_file_generator.py qstream_ack_to_fw_api.xml > files/qstream_ack_to_fw_ipc_server_api.c
-python ../tool/ipc_server_h_file_generator.py qstream_ack_to_fw_api.xml > files/qstream_ack_to_fw_ipc_server_api.h
-python ../tool/ipc_client_c_file_generator.py qstream_ack_to_fw_api.xml > files/qstream_ack_to_fw_ipc_client_api.c
-python ../tool/ipc_client_h_file_generator.py qstream_ack_to_fw_api.xml > files/qstream_ack_to_fw_ipc_client_api.h
